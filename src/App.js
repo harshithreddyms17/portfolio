84b64bfa948d25f591ab2eeff7f27f33c51b5b7e
harshithreddyms17 <harshithreddyms17@gmail.com>
2024-08-30 22:58:21 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import SocialMedia from "./components/SocialMedia";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
 function App() {
   return (
     <div className="overscroll-none">
@@ -11,6 +12,7 @@ function App() {
       <SocialMedia/>
       <About/>
       <Portfolio/>
+      <Experience/>
     </div>
   );
 }
---
+++ src/components/Experience.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div>
+      <p>
+        Harshith
+      </p>
+    </div>
+  )
+}
+
+export default Experience
